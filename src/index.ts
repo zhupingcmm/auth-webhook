691e5bf153018fc3e19da7bd090520d1c896b15a
pingzhu <ping.zhu@microfocus.com>
2022-08-29 16:17:45 +0800

    change

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,8 @@ import { initLog, logFactory } from './log-config.js';
 import { SERVER_PORT } from './config.js';
 import compression from "compression";
 import { Octokit } from '@octokit/core';
-import fetch, {RequestInit} from 'node-fetch';
-import { config } from 'process';
-
-interface Config extends RequestInit {}
 const log = logFactory("startup");
-const octokit = new Octokit({ auth: `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`});
+const octokit = new Octokit({ auth: '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'});
 const start =() => {
     initLog();
     const app = express();
@@ -19,13 +15,9 @@ const start =() => {
 
     app.use('/', async (req, res, next) => {
         try {
-            log.info('request info:', req.query, req.url, req.body);
-            const customerConfig:Config = {
-                method: 'GET',
-                headers: {'Accept': 'application/vnd.github+json', 'Authorization': 'token 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'},
-            }
-            const response = await fetch('https://api.github.com/user', {...customerConfig});
-            req.send(await response.json());
+            const {data} = await octokit.request("/user");
+            log.info('Success get userinfo from github', data);
+            req.send(data);
             next();
         } catch(e) {
             log.error('failed to get userinfo form github %s', e);
